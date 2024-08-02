ca4bc9c8791350aca8a55b44047b6733beedbd26
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-02 14:43:39 +0200

    scaletta

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/*
+SCALETTA:
+
+(Milestone 1) (for + 3 if)
+1- Generare i numeri  da 1 a 100 e li stampo in console
+2- Stampo 'Fizz' al posto dei multipli di 3 (6)
+3- Stampo 'Buzz' al posto dei multipli di 5 (10)
+4- Stampo 'FizzBuzz' al posto dei multipli sia di 3 che di 5 (15)
+
+(Milestone 2)
+6- Creo un contenitore nel DOM
+7- Stampo nel DOM con una tecnica che scelgo (Template literals, InnerHTML) senza CSS
+
+(Milestone 3)
+8- Creo un effetto grafico come da esempio nello screenshot
+
+*/
